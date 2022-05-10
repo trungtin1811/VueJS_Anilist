a0ecd73fd71321abe13ecfcaa5025e6ae52be3c8
trungtin1811 <86908613+trungtin1811@users.noreply.github.com>
2022-05-10 21:29:13 +0700

    final

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./assets/tailwind.css";
-import "boxicons";
-import router from "./router/index";
-
-const app = createApp(App);
-app.use(router);
-app.mount("#app");
